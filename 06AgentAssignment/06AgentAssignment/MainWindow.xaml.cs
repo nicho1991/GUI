c030792321e5f50d1d28872aac7e6fc24a6fccb6
nicho1991 <ladefoged1991@gmail.com>
2018-02-17 18:15:33 +0100

    l

--- 06AgentAssignment/06AgentAssignment/MainWindow.xaml.cs
+++ 06AgentAssignment/06AgentAssignment/MainWindow.xaml.cs
@@ -12,17 +12,31 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using I4GUI;
 
 namespace _06AgentAssignment
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Agent _agent1 = new Agent("007","James Bond", "" , "Assasination" , "Kill the Queen");
+        public Agent _agent2 = new Agent("1337", "perry", "", "cuddling", "make sweet love");
         public MainWindow()
         {
             InitializeComponent();
+            agentlist.Items.Add(_agent1);
+            agentlist.Items.Add(_agent2);
+            //agentlist.Items.Add();
+
+            DataContext = agentlist;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            agentlist.Items.Add(new Agent(IdBox.Text, CodeNameBox.Text, "", SpecialityBox.Text, AssignmentBox.Text));
         }
     }
 }
