4ce46759e0ec8b71bfbb35e25d2c1cefa7d79cba
nicho1991 <ladefoged1991@gmail.com>
2018-03-05 11:27:52 +0100

    fdsfesf

---
+++ 09Settings/09Settings/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _09Settings
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            if (_09Settings.Properties.Settings.Default.CallUpgrade)
+            {
+                _09Settings.Properties.Settings.Default.Upgrade();
+                _09Settings.Properties.Settings.Default.CallUpgrade = false;
+            }
+        }
+
+        private void App_OnExit(object sender, ExitEventArgs e)
+        {
+           // _09Settings.Properties.Settings.Default.Save();
+        }
+    }
+}
