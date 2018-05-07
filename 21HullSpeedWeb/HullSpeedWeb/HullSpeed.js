97b1fbf82d7d49ce083e76c38e516b5e04fc7b52
nicho1991 <ladefoged1991@gmail.com>
2018-05-07 10:08:06 +0200

    Initial forgot

---
+++ 21HullSpeedWeb/HullSpeedWeb/HullSpeed.js
@@ -0,0 +1,15 @@
+﻿// Adjust image to render within window
+document.getElementById("sailboat").height = window.innerHeight - 20;
+
+window.onresize = function () {
+    document.getElementById("sailboat").height = window.innerHeight - 20;
+}
+
+var ship = new Sailboat();
+document.getElementById("calculateHullSpeed").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    ship.setName(document.getElementById("name").value);
+    ship.setLength(parseInt(document.getElementById("length").value));
+    document.getElementById("result").value = ship.hullspeed().toFixed(1);
+});
---
+++ 21HullSpeedWeb/HullSpeedWeb/Sailboat.js
@@ -0,0 +1,30 @@
+﻿function Sailboat() {
+    // Private member variables
+    var name;
+    var length;
+ 
+    // Properties (setters and getters)
+    this.setName = function(value) {
+        name = value;
+    }
+
+    this.getName = function(){
+        return name;
+    }
+
+    this.setLength = function(value) {
+        length = value;
+    }
+
+    this.getLength = function() {
+        return length;
+    }
+
+    // Member functions
+
+    // Calculate the hull speed for the boat.
+    this.hullspeed = function () {
+        return 1.34 * Math.sqrt(length);
+    }
+
+}
